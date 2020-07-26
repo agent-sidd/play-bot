5d0a4ecca453c5929c513315f9c474a1addb2f30
Sidddharth singh <60338719+agent-sidd@users.noreply.github.com>
2020-07-26 21:18:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var modelviewer=document.getElementById('model');
+        function stop(){
+        modelviewer.animationName='none '; 
+        }
+        function run(){
+        modelviewer.animationName='Running';
+        }
+        function wave(){
+        modelviewer.animationName='Wave';
+        }
